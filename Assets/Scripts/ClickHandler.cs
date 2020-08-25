8949ec08f8505df834fb3ac1095268a34bad00b1
Herzogin <s0550233@htw-berlin.de>
2020-08-25 16:18:17 +0200

    initial commit, working demo

---
+++ Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class ClickHandler : MonoBehaviour
+{
+    public UnityEvent UpEvent;
+    public UnityEvent DownEvent;
+    void OnMouseDown()
+    {
+        Debug.Log("Mouse down");
+        DownEvent?.Invoke();
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log("Mouse up");
+        UpEvent?.Invoke();
+    }
+}
