b84b3a99bd9c4b68d540d6d439b13d24b65ffc31
Herzogin <s0550233@htw-berlin.de>
2020-09-18 03:17:49 +0200

    added new menu with buttons

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+	public void switchToScene0()
+	{
+		SceneManager.LoadScene("0_Home");
+	}
+	public void switchToScene1()
+    {
+        SceneManager.LoadScene("1_Positioning");
+    }
+	public void switchToScene2()
+	{
+		SceneManager.LoadScene("2_ObjectIdentification");
+	}
+	public void switchToScene3()
+	{
+		SceneManager.LoadScene("3_InformationMapping");
+	}
+	public void switchToScene4()
+	{
+		SceneManager.LoadScene("4_Disambiguation");
+	}
+}
---
+++ Assets/Scripts/developmentScripts/GazeMenu.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(EventTrigger))]
+public class GazeMenu : MonoBehaviour
+{
+	//public UnityEvent GazeEvent;
+	Vector3 m_start;
+	Vector3 m_target;
+	bool m_targeted = false;
+	public int sceneNumber;
+
+	// Use this for initialization
+	void Start()
+	{
+		// Add triggers.
+		EventTrigger trigger = GetComponent<EventTrigger>();
+
+		// EventTrigger entry for PointerEnter.
+		EventTrigger.Entry enterEntry = new EventTrigger.Entry();
+		enterEntry.eventID = EventTriggerType.PointerEnter;
+		enterEntry.callback.AddListener((data) => { OnPointerEnter((PointerEventData)data); });
+
+		// EventTrigger entry for PointerExit.
+		EventTrigger.Entry exitEntry = new EventTrigger.Entry();
+		exitEntry.eventID = EventTriggerType.PointerExit;
+		exitEntry.callback.AddListener((data) => { OnPointerExit((PointerEventData)data); });
+
+		// Add the entries to the Event Trigger.
+		trigger.triggers.Add(enterEntry);
+		trigger.triggers.Add(exitEntry);
+
+		// Set the initial and target scales.
+		//m_start = transform.localScale;
+		//m_target = m_start * 1.3f;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// Figure out which scale to move towards.
+		//Vector3 target = m_start;
+		if (m_targeted)
+		{
+			//target = m_target;
+			Debug.Log("gazed at");
+			//GazeEvent?.Invoke();
+			StartCoroutine(TimeDelay());
+			switchScene(sceneNumber);
+		}
+
+		// Move towards said scale.
+		//transform.localScale = Vector3.MoveTowards(transform.localScale, target, Time.deltaTime);
+	}
+
+	private void OnPointerEnter(PointerEventData data)
+	{
+		m_targeted = true;
+	}
+
+	private void OnPointerExit(PointerEventData data)
+	{
+		m_targeted = false;
+	}
+
+	void switchScene(int sceneNumber)
+	{
+		if (sceneNumber == 0)
+		{
+			SceneManager.LoadScene("0_Home");
+		}
+		else if (sceneNumber == 1)
+		{
+			SceneManager.LoadScene("1_Positioning");
+
+		}
+		else if (sceneNumber == 2)
+		{
+			SceneManager.LoadScene("2_ObjectIdentification");
+			
+		}
+		else if (sceneNumber == 3)
+		{
+			SceneManager.LoadScene("3_InformationMapping");
+		}
+		else if (sceneNumber == 4)
+		{
+			SceneManager.LoadScene("4_Disambiguation");
+			
+		}
+	}
+
+	IEnumerator TimeDelay()
+	{
+		yield return new WaitForSeconds(3);
+	}
+}
